bbaab3bd27b71d69d15b38ecfbb3cb8d0820119c
yoladango <yoladango@gmail.com>
2017-03-20 00:33:17 -0700

    ssssss

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+/*var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+ GET home page. 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'Dan', noun: 'shit' });
+  res.render('index', { fuck: 'Fuck' });
+
+  
 });
 
-module.exports = router;
+module.exports = router;*/
